86db98d3fb78dfbf01faf0965cb4a42bbb9549e2
Yousef Elsayed <yousefe.elsayed@gmail.com>
2024-05-01 13:37:25 +0300

    Create twitter counter UI Integrate TwTextEditorSDK component

--- TwitterCounter/AppDelegate.swift
+++ TwitterCounter/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import TWTextEditorSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // This registers the fonts
+         registerFonts()
+
         return true
     }
 
---
+++ TwitterCounter/ViewControllers/InitialVC/CustomSplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSplashViewController.swift
+//  TwitterCounter
+//
+//  Created by Yousef Elsayed on 30/04/2024.
+//
+
+import UIKit
+
+class CustomSplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
